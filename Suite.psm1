6f932bc14e81ce01cde6e2ed2448090650803f8b
vyulfherot <vyulfherot@gmail.com>
2026-05-14 01:42:41 +0200

    Added aliases for New-RunShellcode, Conwrite and Pretty-Bytes

--- Suite.psm1
+++ Suite.psm1
@@ -1,1 +1,6 @@
 Get-ChildItem -Path $PSScriptRoot -Recurse -Filter "*.ps*" | ? { $_.FullName -ne $PSCommandPath } | % { Import-Module $_.FullName -DisableNameChecking }
+
+# Aliases
+'nrs', 'nrshc', 'genshc' | % { Set-Alias -Name $_ -Value 'New-RunShellcode' }
+'cw', 'con' | % { Set-Alias -Name $_ -Value 'Conwrite' }
+'pb', 'pbytes' | % { Set-Alias -Name $_ -Value 'Pretty-Bytes' }
